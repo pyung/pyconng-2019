a95dfdedac5a3e0c9ede1075fba4293de8f18652
kelvin <kelvinadigwu@yahoo.com>
2019-06-13 16:04:16 +0100

    ...

--- accounts/models.py
+++ accounts/models.py
@@ -4,15 +4,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-	GENDER = (
-		('M', 'Male'),
-		('F', 'Female'),
-		)
-	OCCUPATION = (
-		('S', 'Student'),
-		('P', 'Professional'),
-		('H', 'Hobbyist')
-		)
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        )
+    OCCUPATION = (
+        ('S', 'Student'),
+        ('P', 'Professional'),
+        ('H', 'Hobbyist')
+        )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     tagline = models.CharField(max_length=35, null=True, blank=True)
