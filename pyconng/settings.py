aef14b7ef77488bd2864e1a5580d704353dbe7ae
miyanda2 <myke1332@gmail.com>
2019-06-16 16:23:39 +0100

    added pycon-ng-test.herokuapp.com to allowed host

--- pyconng/settings.py
+++ pyconng/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY', '#g^v-%vj*lt05hb3vx#-pd3!9=zd4@%6htr_jo8h(sq#3p+b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mighty-brook-77151.herokuapp.com']
+ALLOWED_HOSTS = ['mighty-brook-77151.herokuapp.com', 'pycon-ng-test.herokuapp.com']
 
 
 # Application definition
