e340613415cc28a1aabeaec965c4badaf2938b49
kelvin <kelvinadigwu@yahoo.com>
2019-06-28 13:07:37 +0100

    Update settings.py

--- pyconng/settings.py
+++ pyconng/settings.py
@@ -94,13 +94,16 @@ WSGI_APPLICATION = 'pyconng.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
