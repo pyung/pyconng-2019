6c9a3e406fa4e549f69c13071976df597823f110
Micheal Iyanda <myke1332@gmail.com>
2019-06-29 16:51:24 +0100

    Update settings.py

--- pyconng/settings.py
+++ pyconng/settings.py
@@ -87,7 +87,7 @@ ROOT_URLCONF = 'pyconng.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Before: 'DIRS': ['templates']
+        'DIRS': [BASE_DIR + '/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
