fd58bacfc8d22288329c3366b79359817c01fb04
Micheal Iyanda <myke1332@gmail.com>
2019-07-05 22:17:26 +0100

    Update settings.py

--- pyconng/settings.py
+++ pyconng/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import dotenv
 import pdb
 from django.dispatch import receiver
-from hubspot.signals import payment_verified
+#from hubspot.signals import payment_verified
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
