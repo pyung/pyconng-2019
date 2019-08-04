769fb54473ad51cf91807f1fe9d914afaff2cca8
Abubakar Hassan <abubakarhassan59@gmail.com>
2019-08-04 03:43:50 +0100

    Directory structure update

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-admin.site.site_header = "PYCONNG ADMINISTRATION"
-admin.site.site_title = "PYCONNG Admin Portal"
-admin.site.index_title = "Welcome to PYCONNG ADMINISTRATION"
--- proposals/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Proposal
-
-
-class ProposalForm(forms.ModelForm):
-    class Meta:
-        model = Proposal
-        exclude = ('accepted', 'author','slug', 'submitted')
